166072da24d9cd3002a2402448b12b9802b5cd5d
Dave Shaheen <dave.shaheen@gmail.com>
2018-06-07 14:44:44 -0500

    Add examples for admin directory and token info

--- csharp/Test/Controllers/OAuth2Controller.cs
+++ csharp/Test/Controllers/OAuth2Controller.cs
@@ -1,4 +1,5 @@
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -19,12 +20,34 @@ namespace Test.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<TokenInfo>> Index([FromBody] SignInRequest request)
+        public async Task<ActionResult<string>> Index([FromBody] SignInRequest request)
         {
-            var client = _httpClientFactory.CreateClient();
-            var result = await client.GetStringAsync($"https://www.googleapis.com/oauth2/v3/tokeninfo?id_token={request.Token}");
+            var response = "";
+            var tokenInfoURL = "https://www.googleapis.com/oauth2/v3/tokeninfo";
 
-            return JsonConvert.DeserializeObject<TokenInfo>(result);
+            using (var client = _httpClientFactory.CreateClient())
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var idTokenResult = await client.GetStringAsync($"{tokenInfoURL}?id_token={request.id_token}");
+                var tokenInfo = JsonConvert.DeserializeObject<TokenInfo>(idTokenResult);
+
+                response = $"[{idTokenResult}]";
+
+                if (!string.IsNullOrWhiteSpace(request.access_token))
+                {
+                    var accessTokenResult = await client.GetStringAsync($"{tokenInfoURL}?access_token={request.access_token}");
+
+                    response = $"[{idTokenResult}, {accessTokenResult}]";
+
+                    client.DefaultRequestHeaders.Add("Authorization", $"Bearer {request.access_token}");
+                    var groupInfoResult = await client.GetAsync($"https://www.googleapis.com/admin/directory/v1/groups?userKey={tokenInfo.sub}");
+
+                    response = $"[{idTokenResult}, {accessTokenResult}, {groupInfoResult.Content.ReadAsStringAsync().Result}]";
+                }
+
+                return response;
+            }
         }
     }
 }
--- csharp/Test/Models/SignInRequest.cs
+++ csharp/Test/Models/SignInRequest.cs
@@ -2,6 +2,7 @@ namespace Test.Models
 {
     public class SignInRequest
     {
-        public string Token { get; set; }
+        public string access_token { get; set; }
+        public string id_token { get; set; }
     }
 }
