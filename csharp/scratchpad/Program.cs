13b0bb700466cf6bfc52d718a90c87afd6ad9a4e
Dave Shaheen <dave.shaheen@gmail.com>
2018-08-24 18:04:46 -0400

    Add initial scratchpad project

---
+++ csharp/scratchpad/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace scratchpad
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
