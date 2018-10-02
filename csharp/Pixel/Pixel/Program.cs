b263b751e4e8e319f2f9a4b4418e8ee6c08fc3fc
Dave Shaheen <dave.shaheen@gmail.com>
2018-10-01 22:32:24 -0400

    Update Program

--- csharp/Pixel/Pixel/Program.cs
+++ csharp/Pixel/Pixel/Program.cs
@@ -1,12 +1,32 @@
-using System;
-
 namespace Pixel
 {
     public static class Program
     {
-        public static void Main()
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var logger = new Logger();
+            var animation = new Animation(logger);
+            var tests = new[]
+            {
+                new TestCase { Speed = 2, Init = "..R...." },
+                new TestCase { Speed = 3, Init = "RR..YRY" },
+                new TestCase { Speed = 2, Init = "YRYR.YRYR" },
+                new TestCase { Speed = 10, Init = "RYRYRYRYRY" },
+                new TestCase { Speed = 1, Init = "..." },
+                new TestCase { Speed = 1, Init = "YRRY.YR.YRR.R.YRRY." }
+            };
+
+            foreach (var test in tests)
+            {
+                animation.Animate(test.Speed, test.Init);
+                logger.Log();
+            }
         }
+    }
+
+    public class TestCase
+    {
+        public int Speed { get; set; }
+        public string Init { get; set; }
     }
 }
